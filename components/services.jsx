834adabdcf030d02cce241892a6e6894bc5c335e
SaadJlil <sailjlad@gmail.com>
2023-04-18 04:41:39 +0000

    projects

--- components/services.jsx
+++ components/services.jsx
@@ -5,13 +5,14 @@ import useServiceStore from "@/store/serviceStore"
 export default function Services() {
     const services = useServiceStore((state) => state.services)
     return(
-        <section className={styles.services}>
+        <section className={styles.services} style={{backgroundImage: `url(/home/whoami/Desktop/FrontEnd/PersonalWebsite/public/smartphone-heart-svgrepo-com.svg)`}} >
             <div className={styles.heading}>
                 <h2>My Services</h2>
             </div>
             <div className={styles.slots}>
                 {services.map(service => (
-                    <div key={service.id} className={styles.slot}>
+                    <div key={service.id} className={styles.slot} style={{backgroundImage: `url(${service.img})`}}>
+                        <Image src={service.img} height={200} width={200}/>
                         <div className={styles.slotfade}>
                             <div className={styles.slotTitle}>
                                 <h3>{service.name}</h3>
--- store/serviceStore.jsx
+++ store/serviceStore.jsx
@@ -1,52 +1,51 @@
 import  {create} from 'zustand'
 
-import casatour from '../public/projects/casatour.webp'
-import agoradexed from '../public/projects/agoradex.webp'
-import chemistry from '../public/projects/chemistry.png'
-
-import react from '../public/react.svg'
-import dotnet from '../public/dotnet.svg'
-import postgres from '../public/postgres.svg'
+import interface_img from '../public/smartphone-heart-svgrepo-com.svg'
+import webserver_img from '../public/website-program-svgrepo-com.svg'
+import containerisation_img from '../public/docker-svgrepo-com.svg'
+import database_img from '../public/database-g6a37ecf0b_1280.png'
 
 
 class Service {
-    constructor(id, name, description, github, technologies, img){
+    constructor(id, name, description, img){
         this.id = id;
         this.name = name;
         this.description = description;
+        this.img = img; 
     }
 }
-
-const casaTour = new Service(
+/*
+const interfaces = new Service(
     1,
     "Casa Tour",
     'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius quaerat recusandae sint ratione rerum? Deserunt',
+    '/home/whoami/Desktop/FrontEnd/PersonalWebsite/public/smartphone-heart-svgrepo-com.svg'
 )
 
-const casakhra = new Service(
+const Containerisation = new Service(
     4,
     "Casa Tour",
     'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius quaerat recusandae sint ratione rerum? Deserunt',
+    '../public/smartphone-heart-svgrepo-com.svg'
 )
 
-const chemistery = new Service(
+const Webserver = new Service(
     2,
     "Chemistery",
     'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius quaerat recusandae sint ratione rerum? Deserunt',
+    '../public/dotnet.svg'
 )
-
-const agoradex = new Service(
+*/
+const Database = new Service(
     3,
     "AgoraDEX",
     'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius quaerat recusandae sint ratione rerum? Deserunt',
+    database_img
 )
 
 const useServiceStore = create((set) => ({
     services:  [
-        casaTour,
-        chemistery,
-        agoradex,
-        casakhra
+        Database
     ],
 }))
 

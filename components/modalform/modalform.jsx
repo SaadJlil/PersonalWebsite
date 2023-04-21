b0222a50d9b6e5da54b87345f05850b7284223c0
SaadJlil <sailjlad@gmail.com>
2023-04-21 06:25:14 +0000

    form v2

--- components/modalform/modalform.jsx
+++ components/modalform/modalform.jsx
@@ -1,4 +1,5 @@
 import styles from '../../styles/modalform.module.css'
+import Button_ from '../button_' 
 import Image from "next/image"
 
 export default function Modalform() {
@@ -16,17 +17,22 @@ function Modal() {
         <div className={styles.modalFormFull}>
             <div className={styles.modalContainer}>
                 <div className={styles.modalWin}>
+                        <h2>Hiring Form</h2>
+                    <div className={styles.modalWinForm}>
 
-                    <h2>Hiring Form</h2>
-                    <label>Name</label>
-                    <textarea className={styles.inputform} type="text" />
-                    <label>Organisation</label>
-                    <input className={styles.inputform} type="text" />
-                    <label>Services you want me to be involved in</label>
-                    <input className={styles.inputform} type="text" />
-                    <label>Message</label>
-                    <input className={styles.inputform} type="text" />
+                        <label>Name</label>
+                        <input className={styles.inputform} type="text" />
+                        <label>Organisation</label>
+                        <input className={styles.inputform} type="text" />
+                        <label>Services you want me to be involved in</label>
+                        <input className={styles.inputform} type="text" />
+                        <label>Message</label>
+                        <textarea className={styles.inputform} type="text" />
+                        <div className={styles.ButtonWrapper}>
+                            <Button_ className={styles.button_} link="#" content="Submit" /> 
+                        </div>
 
+                    </div>
                </div>
             </div>
         </div>
